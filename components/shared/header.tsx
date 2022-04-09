a47de246c57509fe177849b5cb73d560d5bddc81
Dan <daniellevenson4@gmail.com>
2022-04-09 12:20:22 -0400

    add header text and divider to header component

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,21 +1,31 @@
 import React from 'react'
 
+import Box from '@mui/material/Box'
+import Divider from '@mui/material/Divider'
 import Tab from '@mui/material/Tab'
 import Tabs from '@mui/material/Tabs'
+import Typography from '@mui/material/Typography'
+import find from 'lodash/find'
 import map from 'lodash/map'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 
 import { tabStyles } from '~/styles/components/shared/styles'
+import { theme } from '~/styles/theme'
 import { linkConfig } from '~/utils/configuration/menu-items'
 import { Link } from '~/utils/types/menu-items'
 
 interface HeaderProps {
+  headerText?: string
   pageTitle: string
   renderTabs?: boolean
 }
 
-const Header: React.FC<HeaderProps> = ({ pageTitle, renderTabs }) => {
+const Header: React.FC<HeaderProps> = ({
+  headerText,
+  pageTitle,
+  renderTabs,
+}) => {
   const classes = tabStyles()
   const router = useRouter()
   const [selectedTab, setSelectedTab] = React.useState<string>('')
@@ -24,6 +34,11 @@ const Header: React.FC<HeaderProps> = ({ pageTitle, renderTabs }) => {
     const selectedPage = router.pathname.split('/').join('')
     if (selectedPage) setSelectedTab(selectedPage)
   }, [router])
+
+  const handleTabChange = (_e: React.SyntheticEvent, value: string) => {
+    const newTab = find(linkConfig, (link) => value === link.url)
+    return router.push(newTab?.url as string)
+  }
 
   return (
     <>
@@ -35,6 +50,7 @@ const Header: React.FC<HeaderProps> = ({ pageTitle, renderTabs }) => {
         {renderTabs && Boolean(selectedTab) && (
           <Tabs
             className={classes.tabs}
+            onChange={handleTabChange}
             value={selectedTab}
             variant="scrollable"
           >
@@ -50,6 +66,16 @@ const Header: React.FC<HeaderProps> = ({ pageTitle, renderTabs }) => {
               )
             })}
           </Tabs>
+        )}
+        {headerText && (
+          <Box mt={5}>
+            <Typography color={theme.palette.secondary.dark} variant="h4">
+              {headerText}
+            </Typography>
+            <Divider
+              sx={{ border: `2px solid ${theme.palette.secondary.dark}` }}
+            />
+          </Box>
         )}
       </div>
     </>
